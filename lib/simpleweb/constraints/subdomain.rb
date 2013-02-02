0c0a5abd33289c945c56b73daaaf69a189fef658
Chris Mytton <self@hecticjeff.net>
2013-02-02 09:47:52 +0000

    Document Subdomain constraint

--- lib/simpleweb/constraints/subdomain.rb
+++ lib/simpleweb/constraints/subdomain.rb
@@ -1,15 +1,15 @@
 module Simpleweb
   module Constraints
     class Subdomain
-      attr_reader :tld_size
+      attr_reader :tld_length
       attr_reader :reserved_subdomains
 
       # Create a new Subdomain constraint.
       #
-      # @param [Integer] tld_size The number of sections after the subdomain.
+      # @param [Integer] tld_length The number of sections after the subdomain.
       # @param [Array] reserved_subdomains Reserved Subdomains
-      def initialize(tld_size, reserved_subdomains = [])
-        @tld_size = tld_size
+      def initialize(tld_length, reserved_subdomains = [])
+        @tld_length = tld_length
         @reserved_subdomains = reserved_subdomains
       end
 
@@ -17,7 +17,7 @@ module Simpleweb
       #
       # @param [Rack::Request] request The request to test for a match.
       def matches?(request)
-        request.subdomain(tld_size).present? && !reserved_subdomains.include?(request.subdomains.first)
+        request.subdomain(tld_length).present? && !reserved_subdomains.include?(request.subdomains.first)
       end
     end
   end
