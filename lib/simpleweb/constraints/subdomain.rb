f83ba12b39be7ff59a20e8fcca75930707584c2c
Chris Mytton <self@hecticjeff.net>
2013-02-02 13:27:09 +0000

    Don't need to specify full namespace here

--- lib/simpleweb/constraints/subdomain.rb
+++ lib/simpleweb/constraints/subdomain.rb
@@ -2,7 +2,7 @@ module Simpleweb
   module Constraints
 
     def subdomain(*args, &block)
-      constraints Simpleweb::Constraints::Subdomain.new(*args), &block
+      constraints Subdomain.new(*args), &block
     end
 
     class Subdomain
