bcfd715361d0ebf1c507775ef2d67bb81c15c3c7
Chris Mytton <self@hecticjeff.net>
2013-02-02 11:23:27 +0000

    Add subdomain method to constraints module

--- lib/simpleweb/constraints.rb
+++ lib/simpleweb/constraints.rb
@@ -1,1 +1,9 @@
 require 'simpleweb/constraints/subdomain'
+
+module Simpleweb
+  module Constraints
+    def subdomain(*args, &block)
+      constraints Simpleweb::Constraints::Subdomain.new(*args), &block
+    end
+  end
+end
